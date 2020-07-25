4075eb70406ebf46843a22ccba5c3ab62b5907ed
anu radha <anu.sivasubramanian@gmail.com>
2020-07-25 23:10:23 +0200

    first commit

---
+++ public/admin/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <title>TB</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/admin.css" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <h1>Tech Blog Admin Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <div id="content"></div>
+    <footer>
+        &copy; <?php echo date('Y'); ?> Tech Blog</footer>
+</body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <title>Tech Blog</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>Tech Blog coming soon!!</h1>
+</body>
+
+</html>
